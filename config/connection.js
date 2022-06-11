6d377e4f11a41e2d2b828acc069d6f0dc0c7d8be
bdurushia <bdurushia@gmail.com>
2022-06-11 09:28:45 -0500

    saving

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
+console.log(process.env);
 
 const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PASSWORD', {
   host: 'localhost',
